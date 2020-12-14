ebc8d6a592810f41b2aad70297f6e100c7774211
mikayla-bryant <mikaylabryant25@gmail.com>
2020-12-14 16:50:38 -0500

    Looped through characters list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './App.css';
-import axios from 'axios'
+import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,25 +9,12 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-  const [characters, setCharacterList] = useState([]);
-    
-    useEffect(() =>{
-        //Requesting Data from NASA's API
-        axios.get("https://swapi.dev/api/people/")
-             .then((response) => {
-                 console.log('success!', response);
-                 setCharacterList(response.data.results);
-  
-             })
-             // 
-             .catch((error) => {
-                 console.log("Whoops. There was an error retrieving your data. Please check your work.", error);
-             })
-    }, [])
+ 
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character/>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,30 @@
 // Write your Character component here
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+export default function Character() {
+  const [characters, setCharacterList] = useState([]);
+
+  useEffect(() => {
+    //Requesting Data from Star Wars API
+    axios
+      .get('https://swapi.dev/api/people/')
+      .then((response) => {
+        console.log('success!', response);
+        setCharacterList(response.data.results);
+      })
+      .catch((error) => {
+        console.log(
+          'Whoops. There was an error retrieving your data. Please check your work.',
+          error
+        );
+      });
+  }, []);
+  console.log(characters);
+  return (
+    <div>
+      {characters.map((item) => {
+        return <h2>{item.name}</h2>;
+      })}
+    </div>
+  );
+}
