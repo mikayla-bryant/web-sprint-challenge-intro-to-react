83d36e1ebdd0f9b88aed53a0a18203dd10a324da
mikayla-bryant <mikaylabryant25@gmail.com>
2020-12-14 17:15:56 -0500

    Added styling components

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,6 +1,15 @@
 // Write your Character component here
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
+const Card = styled.div`
+  background-color: #faf2cd;
+  width: 40%;
+  margin: auto auto 20px auto;
+  padding: 5%;
+  font-family: 'Piazzolla', serif;
+`;
+
 export default function Character() {
   const [characters, setCharacterList] = useState([]);
 
@@ -19,11 +28,20 @@ export default function Character() {
         );
       });
   }, []);
+
   console.log(characters);
   return (
     <div>
       {characters.map((item) => {
-        return <h2>{item.name}</h2>;
+        return (
+          <Card>
+            <h2>{item.name}</h2>
+            <p>
+              This character is of the {item.gender} gender, was born in{' '}
+              {item.birth_year}, and has {item.eye_color} eyes.
+            </p>
+          </Card>
+        );
       })}
     </div>
   );
